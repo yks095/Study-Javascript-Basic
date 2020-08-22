605a117bcbd9764027955515b5835ac1c945ed61
kiseok, Yang <rltjr219@gmail.com>
2020-08-22 20:01:44 +0900

    배열에 항목 추가하기

--- react-tutorial/begin-react/src/App.js
+++ react-tutorial/begin-react/src/App.js
@@ -1,22 +1,54 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 import UserList from './UserList';
+import CreateUser from './CreateUser';
 
 function App() {
-  const users = [
+  const [inputs, setInputs] = useState({
+    username: '',
+    email: ''
+  });
+
+  const { username, email } = inputs;
+  const onChange = e => {
+    const { name, value } = e.target;
+    setInputs({
+      ...inputs,
+      [name]: value
+    });
+  };
+
+  const [ users, setUsers ] = useState([
     {id: 1, username: "test1", email: "test1@email.com"},
     {id: 2, username: "test2", email: "test2@email.com"},
     {id: 3, username: "test3", email: "test3@email.com"},
-];
+]);
 
   const nextId = useRef(4);
 
   const onCreate = () => {
-    console.log(nextId.current);
-    nextId += 1;
+    const user = {
+      id: nextId.current,
+      username,
+      email
+    };
+    setUsers([...users, user]);
+    setInputs({
+      username: '',
+      email: ''
+    });
+    nextId.current += 1;
   }
 
   return (
-  <UserList users={users}/>
+    <>
+      <CreateUser 
+        username={username} 
+        email={email} 
+        onCreate={onCreate} 
+        onChange={onChange}
+      />
+      <UserList users={users}/>
+    </>
   );
 }
  
