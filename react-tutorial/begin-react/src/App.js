ca8f5e31161119d5cd771e78f9f9760b2ec51c4d
kiseok, Yang <rltjr219@gmail.com>
2020-08-24 19:18:55 +0900

    useCallback을 사용하여 함수 재사용 하기

--- react-tutorial/begin-react/src/App.js
+++ react-tutorial/begin-react/src/App.js
@@ -1,4 +1,4 @@
-import React, { useRef, useState, useMemo } from 'react';
+import React, { useRef, useState, useMemo, useCallback } from 'react';
 import UserList from './UserList';
 import CreateUser from './CreateUser';
 
@@ -14,13 +14,13 @@ function App() {
   });
 
   const { username, email } = inputs;
-  const onChange = e => {
+  const onChange = useCallback(e => {
     const { name, value } = e.target;
     setInputs({
       ...inputs,
       [name]: value
     });
-  };
+  }, [inputs]);
 
   const [ users, setUsers ] = useState([
     {id: 1, username: "test1", email: "test1@email.com", active: true},
@@ -30,7 +30,7 @@ function App() {
 
   const nextId = useRef(4);
 
-  const onCreate = () => {
+  const onCreate = useCallback(() => {
     const user = {
       id: nextId.current,
       username,
@@ -42,19 +42,19 @@ function App() {
       email: ''
     });
     nextId.current += 1;
-  };
+  }, [username, email, users]);
 
-  const onRemove = id => {
+  const onRemove = useCallback(id => {
     setUsers(users.filter(user => user.id !== id));
-  };
+  }, [users]);
 
-  const onToggle = id => {
+  const onToggle = useCallback(id => {
     setUsers(users.map(
       user => user.id === id
         ? { ...user, active: !user.active} 
         : user
     ));
-  };
+  }, [users]);
 
   const count = useMemo(() => countActiveUsers(users), [users]);
 
