dc2ffc43b347c4b60108ee73942f452d2aec0bdf
kiseok, Yang <rltjr219@gmail.com>
2020-09-04 15:11:40 +0900

    Action 객체를 기반으로 상태를 업데이트 하는 useReducer

--- react-tutorial/begin-react/src/Counter.js
+++ react-tutorial/begin-react/src/Counter.js
@@ -1,20 +1,40 @@
-import React, { useState } from 'react'
+import React, { useReducer } from 'react'
+
+function reducer(state, action) {
+  switch (action.type)  {
+    case 'INCREMENT':
+      return state + 1;
+    case 'DECREMENT':
+      return state - 1;
+    default:
+      throw new Error('Unhandled action');
+  }
+
+}
 
 function Counter()  {
-    const [number, setNumber] = useState(0);      // useState를 호출하면 배열을 반환
-    const onIncrease = () => {                    // 첫번째 파라미터는 현재 상태, 두번째 파라미터는 현재 상태를 바꿔주는 함수
-        setNumber(prevNumber => prevNumber + 1);
-      }
-      const onDecrease = () => {
-        setNumber(prevNumber => prevNumber - 1);
-      }
-    return (
+    
+  const [number, dispatch] = useReducer(reducer, 0);
+
+  const onIncrease = () => {                   
+    dispatch({
+      type: 'INCREMENT'
+    })
+  };
+  
+  const onDecrease = () => {
+    dispatch({
+      type: 'DECREMENT'
+    })
+  };
+
+  return (
         <div>
             <h1>{number}</h1>
             <button onClick={onIncrease}>+1</button>
-            <button onClick={onIncrease}>-1</button>
+            <button onClick={onDecrease}>-1</button>
         </div>
-    );
+  );
 }
 
 export default Counter;
